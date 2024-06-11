2dec860b6a8762e7a0bb2ea536d2727390d67dd3
Leonardo Llanes <leonardollanes@Leonardos-Mac-Studio.local>
2024-06-10 23:40:39 -0700

    tech and devTools

--- src/Components/Body/Intro.jsx
+++ src/Components/Body/Intro.jsx
@@ -9,8 +9,7 @@ const Intro = () =>{
         <div className="  container w-full mx-auto flex flex-row flex-wrap justify-center items-start h-1/5  pt-10 md:justify-center md:h-1/2 lg:justify-around lg:flex-nowrap lg:items-center lg:pt-10 lg:h-auto lg:py-10">
             <div className="text-center w-1/2">
                 <h1 className=" text-white text-4xl md:text-6x1 lg:text-6xl">Leonardo LLanes</h1>
-                <p className="text-white text-base"><b>Phone: </b>520-369-0754</p>
-                <p className="text-white text-base"><b>Email: </b>llanesleonardo@gmail.com</p>
+                <p className="text-white text-base"><b>Email: </b>llanesleonardo.developer@gmail.com</p>
             </div>
             <div className="mx-auto w-1/2">
                 <img src={Globe} className="  w-1/3 hidden md:w-1/3 md:block md:mx-auto md:py-5 lg:flex-nowrap lg:justify-evenly lg:w-1/3 lg:block" />
